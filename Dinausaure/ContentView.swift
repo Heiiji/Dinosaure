2d5f0d6ee2711bf54556f93b3f01c4497b95d748
Julien Juret <julien.juret@gmail.com>
2023-06-29 19:38:42 +0200

    Initial Commit

---
+++ Dinausaure/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dinausaure
+//
+//  Created by Julien Juret on 29/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dinausaure/DinausaureApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinausaureApp.swift
+//  Dinausaure
+//
+//  Created by Julien Juret on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct DinausaureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
